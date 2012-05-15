232cc1d468748795339fbeab501f6569bacda36b
Isaiah DeRose-Wilson <isaiah@synapsestudios.com>
2012-05-15 13:50:35 -0700

    Fix issue with duplicate file names and update all files to follow PSR-0 standards.

--- application/classes/Controller/Main.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct script access.');
-
-class Controller_Main extends Abstract_Controller_Website {
-
-	public function action_index() {}
-
-}
--- application/classes/view/page/main/index.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct script access.');
-
-class View_Page_Main_Index extends Abstract_View_Page {}
