41ae6163f585af37889feb51cd8fc98c59b2c0ac
Isaiah DeRose-Wilson <isaiah@synapsestudios.com>
2012-05-16 10:33:14 -0700

    Move CSRF class into project template and remove csrf submodule.

---
+++ application/classes/CSRF.php
@@ -0,0 +1,35 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+
+class CSRF {
+
+	/**
+	 * Returns the token in the session or generates a new one
+	 *
+	 * @param   string  $namespace - semi-unique name for the token (support for multiple forms)
+	 * @return  string
+	 */
+	public static function token()
+	{
+		$token = Session::instance()->get('csrf-token');
+
+		// Generate a new token if no token is found
+		if ( ! $token)
+		{
+			$token = Text::random('alnum', rand(20, 30));
+			Session::instance()->set('csrf-token', $token);
+		}
+
+		return $token;
+	}
+
+	/**
+	 * Validation rule for checking a valid token
+	 *
+	 * @param   string  $token - the token string to check for
+	 * @return  bool
+	 */
+	public static function valid($token)
+	{
+		return $token === self::token();
+	}
+}
